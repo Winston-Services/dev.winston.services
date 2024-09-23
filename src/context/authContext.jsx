0afcd0069742fafdf44dbb22858daa0d996105d9
Michael A. Dennis <michaeladennis@yahoo.com>
2024-09-23 13:21:20 -0500

    testing messages build

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -118,7 +118,7 @@ export function AuthProvider({ children }) {
     };
   });
 
-  const value = { authenticated: auth, setAuth: addAuth, removeAuth, connection };
+  const value = { authenticated: auth, setAuth: addAuth, removeAuth, get connection() { return connection.current } };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 AuthProvider.propTypes = {
