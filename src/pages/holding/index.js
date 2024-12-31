b587c567c4436136c10d58a541ab4232111ea1ec
dreamingrainbow <michaeladennis@yahoo.com>
2024-12-31 00:14:28 -0600

    updating asset listing

--- src/pages/holding/index.js
+++ src/pages/holding/index.js
@@ -8,6 +8,7 @@ import {
   Divider,
 } from '@mui/material';
 import { JsonRpcProvider, Contract, formatUnits } from 'ethers';
+import PropTypes from 'prop-types';
 
 import Table from '../../components/common/Table';
 
@@ -38,24 +39,12 @@ const columns = [
   },
 ];
 
-const useBalance = () => {
+const useBalance = (initialTokens) => {
   const [address, setAddress] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(false);
   const [balances, setBalances] = React.useState([]);
   const [error, setError] = React.useState(null);
-
-  const tokens = React.useMemo(
-    () => [
-      '0x55d398326f99059fF775485246999027B3197955', // USDT
-      '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c', // WBNB
-      '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82', // CAKE
-      '0x75578ebbefe274f240b8e1b5859ca34f342157d9', // WIN
-      '0xeca15e1bbff172d545dd6325f3bae7b737906737', // RKL
-      '0x181d5cec845973e64bccf9848412948be8a3883e', // WAC
-      '0x3A81caafeeDCF2D743Be893858cDa5AcDBF88c11', // AHWA
-    ],
-    []
-  );
+  const [tokens, setTokens] = React.useState(initialTokens);
 
   const fetchTokens = React.useCallback(
     async (network = 'https://bsc-dataseed.binance.org/') => {
@@ -82,24 +71,23 @@ const useBalance = () => {
         'function symbol() view returns (string)',
       ];
 
-      const tokenData = await Promise.all(
-        tokens.map(async (tokenAddress) => {
-          const tokenContract = new Contract(tokenAddress, abi, provider);
-          const balance = await tokenContract.balanceOf(address);
-          const decimals = await tokenContract.decimals();
-          const name = await tokenContract.name();
-          const symbol = await tokenContract.symbol();
-          return {
-            contractAddress: tokenAddress,
-            tokenAddress,
-            balance: balance.toString(),
-            decimals: decimals,
-            symbol,
-            name,
-          };
-        })
-      );
-      // console.log(tokenData);
+      const tokenData = [];
+      for (const tokenAddress of tokens) {
+        await new Promise(resolve => setTimeout(resolve, 200));
+        const tokenContract = new Contract(tokenAddress, abi, provider);
+        const balance = await tokenContract.balanceOf(address);
+        const decimals = await tokenContract.decimals();
+        const name = await tokenContract.name();
+        const symbol = await tokenContract.symbol();
+        tokenData.push({
+          contractAddress: tokenAddress,
+          tokenAddress,
+          balance: balance.toString(),
+          decimals: decimals,
+          symbol,
+          name,
+        });
+      }
 
       setBalances(tokenData);
       setIsLoading(false);
@@ -114,19 +102,22 @@ const useBalance = () => {
     setAddress,
     isLoading,
     error,
+    setTokens,
   };
 };
 
-function TrustMultisig() {
-  const trustMultisigAddress = '0x40e4Da770530E960AE671634d14625F29e3dDb12';
-
-  const { balances, fetchTokens, setAddress, isLoading, error } = useBalance();
+function TrustWalletSection({ walletAddress, walletName, tokenList }) {
+  const { balances, fetchTokens, setAddress, isLoading, error, setTokens } = useBalance();
 
   const [rows, setRows] = React.useState([]);
 
   React.useEffect(() => {
-    setAddress(trustMultisigAddress);
-  }, [setAddress, trustMultisigAddress]);
+    setTokens(tokenList);
+  }, [setTokens, tokenList]);
+
+  React.useEffect(() => {
+    setAddress(walletAddress);
+  }, [setAddress, walletAddress]);
 
   React.useEffect(() => {
     fetchTokens();
@@ -150,7 +141,7 @@ function TrustMultisig() {
   return (
     <>
       <Typography textAlign={'center'} variant="h3">
-        Multisig Trust Holdings
+        {walletName}
       </Typography>
       <Grid mt={5}>
         <Card elevation={0} sx={{ pb: 7 }}>
@@ -167,62 +158,92 @@ function TrustMultisig() {
   );
 }
 
-function TrustHotWalletHoldings() {
-  const trustMultisigAddress = '0xf9b9ee3b0301b511cd5aa4b8d039f63df19c615a';
+TrustWalletSection.propTypes = {
+  walletAddress: PropTypes.string.isRequired,
+  walletName: PropTypes.string.isRequired,
+  tokenList: PropTypes.array.isRequired,
+};
 
-  const { balances, fetchTokens, setAddress, isLoading, error } = useBalance();
-
-  const [rows, setRows] = React.useState([]);
-
-  React.useEffect(() => {
-    setAddress(trustMultisigAddress);
-  }, [setAddress, trustMultisigAddress]);
-
-  React.useEffect(() => {
-    fetchTokens();
-  }, [fetchTokens]);
-
-  React.useEffect(() => {
-    setRows(
-      balances.map((balance, index) => ({
-        id: index,
-        coinName: balance.name,
-        TokenBalance: formatUnits(balance.balance, balance.decimals),
-        price: '--',
-        holdingValue: 0,
-        decimals: balance.decimals.toString(),
-        contractAddress: balance.contractAddress,
-        more: <Typography>More</Typography>,
-      }))
-    );
-  }, [balances]);
-
-  return (
-    <>
-      <Typography textAlign={'center'} variant="h3">
-        Trust Hot Wallet Holdings
-      </Typography>
-      <Grid mt={5}>
-        <Card elevation={0} sx={{ pb: 7 }}>
-          {error ? (
-            <p>Error: {error.message}</p>
-          ) : isLoading ? (
-            <p>Loading...</p>
-          ) : (
-            <Table columns={columns} rows={rows}></Table>
-          )}
-        </Card>
-      </Grid>
-    </>
-  );
-}
 
 function Holding() {
+  const trustMultisigAddress = '0x40e4Da770530E960AE671634d14625F29e3dDb12';
+  const trustHotWalletAddress = '0xf9b9ee3b0301b511cd5aa4b8d039f63df19c615a';
+  const winstonContractAddress = '0x75578ebbefe274f240b8e1b5859ca34f342157d9';
+  const tokenList1 = [
+    '0x75578ebbefe274f240b8e1b5859ca34f342157d9', // WIN
+    '0x55d398326f99059fF775485246999027B3197955', // USDT
+    '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c', // BTCB
+    '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c', // WBNB
+    '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82', // CAKE
+    '0xeca15e1bbff172d545dd6325f3bae7b737906737', // RKL
+    '0x181d5cec845973e64bccf9848412948be8a3883e', // WAC
+    '0x3A81caafeeDCF2D743Be893858cDa5AcDBF88c11', // AHWA
+    '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d', // USDC
+    '0xB2a13cE9f3dFEb5F38F2211FbF3dE91D72CdABDD', // Cake/RKL LP
+  ];
+
+  const tokenList2 = [
+    '0x75578ebbefe274f240b8e1b5859ca34f342157d9', // WIN
+    '0x55d398326f99059fF775485246999027B3197955', // USDT
+    '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c', // WBNB
+    '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82', // CAKE
+    '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c', // BTCB
+    '0xeca15e1bbff172d545dd6325f3bae7b737906737', // RKL
+    '0x181d5cec845973e64bccf9848412948be8a3883e', // WAC
+    '0x3A81caafeeDCF2D743Be893858cDa5AcDBF88c11', // AHWA
+    '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d', // USDC
+    '0xCC42724C6683B7E57334c4E856f4c9965ED682bD', // WMATIC
+    '0xB2a13cE9f3dFEb5F38F2211FbF3dE91D72CdABDD', // Cake/RKL LP
+    '0x4C24b7a71E7576d6623F62bb83AF63F31230dDFc', // Useless/Rkl LP
+    '0x50fB90305D77AdFB3d7473C1661Fb157C6a4eeD4', // USD/WIN LP
+    '0x656Fb87cda1F25e8181b0415ef65dd6D439165e4', // BTCB/RKL LP
+    '0xe01d99478B8a159b957d75455C01104734E05eE1', // USD/RKL LP
+    '0x252853aa8a9509a1c0ad5170B67d25328e3Ef648', // USDC/WIN LP
+    '0x9E49C98Bd667A8b06Fff26ddFCbB9e5DBc0eCD60', // WBNB/WIN LP
+    '0xC087C78AbaC4A0E900a327444193dBF9BA69058E', // USDC/BUSD LP
+    '0x157197baf3E455548987dF50f87846EA17F7B7Ab', // WBNB/WIN LP
+    '0xD8cB5C5baAc62c1e0E79246AeC4DCD52136B599C', // USDC/WAC LP
+    '0x8820dF1B7f854773a694daB3D30247f6814c64aA', // BTCB/WIN LP
+    '0xBb22D416AC55E8D07d9fE78dA3cC9919A1c357ce', // RKL/WIN LP
+  ];
+  const tokenList3 = [
+    '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c', // BTCB
+    '0x75578ebbefe274f240b8e1b5859ca34f342157d9', // WIN
+    '0xeca15e1bbff172d545dd6325f3bae7b737906737', // RKL
+    '0x181d5cec845973e64bccf9848412948be8a3883e', // WAC
+    '0x3A81caafeeDCF2D743Be893858cDa5AcDBF88c11', // AHWA
+    '0xB2a13cE9f3dFEb5F38F2211FbF3dE91D72CdABDD', // Cake/RKL LP
+    '0x4C24b7a71E7576d6623F62bb83AF63F31230dDFc', // Useless/Rkl LP
+    '0x50fB90305D77AdFB3d7473C1661Fb157C6a4eeD4', // USD/WIN LP
+    '0x656Fb87cda1F25e8181b0415ef65dd6D439165e4', // BTCB/RKL LP
+    '0xe01d99478B8a159b957d75455C01104734E05eE1', // USD/RKL LP
+    '0x252853aa8a9509a1c0ad5170B67d25328e3Ef648', // USDC/WIN LP
+    '0x9E49C98Bd667A8b06Fff26ddFCbB9e5DBc0eCD60', // WBNB/WIN LP
+    '0xC087C78AbaC4A0E900a327444193dBF9BA69058E', // USDC/BUSD LP
+    '0x157197baf3E455548987dF50f87846EA17F7B7Ab', // WBNB/WIN LP
+    '0xD8cB5C5baAc62c1e0E79246AeC4DCD52136B599C', // USDC/WAC LP
+    '0x8820dF1B7f854773a694daB3D30247f6814c64aA', // BTCB/WIN LP
+    '0xBb22D416AC55E8D07d9fE78dA3cC9919A1c357ce', // RKL/WIN LP
+  ];
   return (
     <Container>
-      <TrustMultisig />
+      <TrustWalletSection
+        walletAddress={trustMultisigAddress}
+        walletName="Multisig Trust Holdings"
+        tokenList={tokenList1}
+      />
       <Divider sx={{ mt: 5, mb: 5 }} />
-      <TrustHotWalletHoldings />
+      <TrustWalletSection
+        walletAddress={trustHotWalletAddress}
+        walletName="Caretaker Hot Wallet"
+        tokenList={tokenList2}
+      />
+      <Divider sx={{ mt: 5, mb: 5 }} />
+      <TrustWalletSection
+        walletAddress={winstonContractAddress}
+        walletName="Winston Contract"
+        tokenList={tokenList3}
+      />
     </Container>
   );
 }
